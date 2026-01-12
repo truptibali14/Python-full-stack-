5b03114cb16edbbe388e2e09fac0e36ed839f389
truptibali14 <truptibali2003@gmail.com>
2026-01-12 13:55:45 +0530

    Add files via upload

---
+++ order.py
@@ -0,0 +1,5 @@
+Item_prices=[10,67,45,38,88]
+Total_order_value=sum(Item_prices)
+Average_price=Total_order_value/len(Item_prices)
+print("Total_order_value",Total_order_value)
+print("Average_price",Average_price)
