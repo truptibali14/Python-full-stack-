6b58d4e437104402e9536741dc722dc3f0ce8de2
truptibali14 <truptibali2003@gmail.com>
2026-01-12 13:56:28 +0530

    Add files via upload

---
+++ Product.py
@@ -0,0 +1,5 @@
+product_category={"Laptop":"Electronics","Chair":"Furniture","Moblie":"Electronics"}
+print("product_name",list(product_category.keys()))
+print("category",list(product_category.values()))
+total_product=len(product_category)
+print("Total_product",total_product)
